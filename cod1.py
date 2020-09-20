c470a7322365cf3d34254ba187b7dddf36c95a27
Alvaro <afmuente@outlook.com>
2020-09-20 02:41:18 -0500

    Primer commit

---
+++ cod1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data = pd.read_json('hdro_api_all.json')
+
+listapaises = data.country_code
+listapaises = listapaises.drop_duplicates()
+listapaises = listapaises.reset_index(drop=True)
+
+listavar = data.indicator_name
+listavar = listavar.drop_duplicates()
+listavar = listavar.reset_index(drop=True)
+
+# table[table.column_name == some_value]
+dataper = data[data.country_code == 'PER']
+dataper1990 = dataper[dataper.year == 1990]
+dataper2018 = dataper[dataper.year == 2018]
+
+datausa = data[data.country_code == 'USA']
+datausa2018 = datausa[datausa.year == 2018]
+
+data.to_csv ('data.csv', index = True, header=True)
---
+++ cod2.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+data = pd.read_csv('datachevere.csv')
+data = data[data.year != 2020]
+
+data = data.sort_values(by=['country_code', 'year'])
+data = data.reset_index(drop=True)
+
+listap = data.country_code
+listap = listap.drop_duplicates()
+listap = listap.reset_index(drop=True)
+
+dataxp = []
+for i in listap:
+    temp = data[data.country_code == i]
+    dataxp.append(temp)
+
+# for i in range(len(dataxp)):
+#     if not dataxp[i].shape == (29, 149):
+#         print(data['country_code'][0:1]
+
+listafinal = []
+for pais in dataxp:
+    for column in pais.columns[3:]:
+
+    # print(i[0:1])
+
+    # print(i[5:6])
+    # print(i[10:11])
+    # print(i[15:16])
+    # print(i[20:21])
